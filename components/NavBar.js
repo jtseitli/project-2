401aa76cc0a21fbd9a1259b726ba027d10bd0189
Your Name <you@example.com>
2025-10-31 11:28:28 -0400

    Initial commit - Next.js project with NavBar

---
+++ components/NavBar.js
@@ -0,0 +1,9 @@
+export default function NavBar() {
+    return (
+      <nav style={{ padding: '1rem', background: '#fff' }}>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+      </nav>
+    )
+  }
+  
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to My Project!</h1>
+    </main>
+  )
+}
+
